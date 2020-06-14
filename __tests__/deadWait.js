aecdf60f8beae96a6b0c090b11791a1b2bc422cd
unknown <Dustin.Guillen14@yahoo.com>
2020-06-14 11:55:41 -0600

    added edge cases

--- __tests__/deadWait.js
+++ __tests__/deadWait.js
@@ -5,7 +5,7 @@ const { add } = require('../calculator/calculator');
 
 describe("all tests", () => {
     it("should be running tests", function () {
-        expect(true).toBe(true)
+        expect(true).toBeTruthy()
     })
 
     describe("user OBJ", () => {
@@ -20,12 +20,73 @@ describe("all tests", () => {
     describe("the calculator", () => {
         it("returns the sum of two given numbers", function () {
             expect(add(2, 2)).toBe(4);
-            expect(add(12, 2)).toBe(14);
-            expect(add(9, 99)).toBe(108);
-            expect(add(400, 20)).toBe(420);
+            expect(add(-2, -2)).toBe(-4);
+            expect(add(2, -2)).toBe(0);
+            expect(add(0, 2)).toBe(2);
+            expect(add(0, 0)).toBe(0);
+        
         })
+
         it("returns 0 if no numbers are given", function(){
             expect(add()).toBe(0);
+        })
+
+        it("should return the value of the argument when only one number is given", function(){
+            expect(add(0)).toBe(0);
+            expect(add(2)).toBe(2);
+            expect(add(undefined, 2)).toBe(2);
+        })
+
+        it("should return an error when arguments are not numbers", function(){
+            expect(() => {
+                add({}, {})
+            }).toThrow();
+            expect(() => {
+                add(2, {})
+            }).toThrow();
+            expect(() => {
+                add({}, 2)
+            }).toThrow();
+
+            expect(() => {
+                add([], [])
+            }).toThrow();
+            expect(() => {
+                add(2, [])
+            }).toThrow();
+            expect(() => {
+                add([], 2)
+            }).toThrow();
+
+            expect(() => {
+                add(NaN, NaN)
+            }).toThrow();
+            expect(() => {
+                add(2, NaN)
+            }).toThrow();
+            expect(() => {
+                add(NaN, 2)
+            }).toThrow();
+
+            expect(() => {
+                add(null, null)
+            }).toThrow();
+            expect(() => {
+                add(2, null)
+            }).toThrow();
+            expect(() => {
+                add(null, 2)
+            }).toThrow();
+
+            expect(() => {
+                add("two", "two")
+            }).toThrow();
+            expect(() => {
+                add(2, "two")
+            }).toThrow();
+            expect(() => {
+                add("two", 2)
+            }).toThrow();
         })
 
         it.todo("make more tests");
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,5 +3,14 @@ module.exports = {
 }
 
 function add(a = 0, b = 0){
-    return a + b;
+    if(isNumber(a) && isNumber(b)){
+        return a + b;
+    } else {
+        throw new Error("Both arguments need to be numbers")
+    }
+    
+}
+
+function isNumber(value){
+    return !isNaN(parseFloat(value)) && isFinite(value);
 }
